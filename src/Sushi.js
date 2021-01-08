8ea80262fe1f5c2075ff638c5d43e934845e212b
Brandon Austin <61210295+branaus@users.noreply.github.com>
2021-01-08 13:21:45 -0800

    sushi

--- src/Sushi.js
+++ src/Sushi.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './Sushi.css'
+import Sushi1 from './Sushi1.png'
+import Sushi2 from './Sushi2.png'
 
 class Sushi extends Component {
     render() {
@@ -8,6 +10,16 @@ class Sushi extends Component {
                 <h1>
                     SUSHHHH
                 </h1>
+                <img
+                    className="Sushi1-img"
+                    src={Sushi1}
+                    alt="Sushi"
+                />
+                <img
+                    className="Sushi2-img"
+                    src={Sushi2}
+                    alt="Sushi"
+                />
             </div >
         )
     }
